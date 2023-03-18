9799ac678adb2d47623d4260cdf8d706ed4f367a
shreecheryl <shreecheryl@att.net>
2023-03-18 13:27:23 -0700

    row and cells created

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
 // GET ADD EMPLOYEE FORM AND EMPLOYEE TABLE FROM THE DOM
+const $ = (id) => document.getElementById(id);
+let form = $("addForm");
+let table = $("employees");
 
 // SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
-
+let count = 0;
 
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
+    e.preventDefault();
 
     // GET THE VALUES FROM THE TEXT BOXES
-
+    let employeeID = $("id").value;
+    let fullName = $("name").value;
+    let extension = $("extension").value;
+    let email = $("email").value;
+    let department = $("department").value;
+    
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
+    let row = table.insertRow();
 
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
+    let cellID = row.insertCell();
+    let cellName = row.insertCell();
+    let cellExtension = row.insertCell();
+    let cellEmail = row.insertCell();
+    let cellDepartment = row.insertCell();
 
     // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
 
